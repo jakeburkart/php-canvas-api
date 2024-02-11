c523224bf90477dbca4de63a6391e2f74ed02642
Jake Burkart <jake.burkart@gmail.com>
2024-02-10 19:40:14 -0800

    Update CanvasApiException.php
    Removed Error: before response body.

--- src/LonghornOpen/CanvasApi/CanvasApiException.php
+++ src/LonghornOpen/CanvasApi/CanvasApiException.php
@@ -11,7 +11,7 @@ class CanvasApiException extends RuntimeException
     public function __construct($http_status_code, $response_body)
     {
         $this->response_body = $response_body;
-        $message = 'Error ' . $http_status_code . ': ' . $response_body;
+        $message = $response_body;
         parent::__construct($message, $http_status_code);
     }
 }
